145ddfa1c3444b71b34260a3d0dd1d6c51d2ef4f
Tejeswara maddina <teja.rao112@gmail.com>
2022-08-30 10:32:36 -0400

    Add files via upload

---
+++ swapNo.java
@@ -0,0 +1,17 @@
+package practiseJava;
+
+public class swapNo {
+	public static void main(String[] args) {
+		int a=10,b=20;
+	System.out.println("initial value of a is:" +a);
+	System.out.println("initial value of b is:" +b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+	System.out.println("value of a after swaping is " +a);
+	System.out.println("value of b after swaping is "+b);
+		
+		
+	
+	}
+}
